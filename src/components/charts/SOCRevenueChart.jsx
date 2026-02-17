10e96f56013073358c5cded379beb264a53a2120
unknown <aliezzati@greenely.se>
2026-02-17 14:57:31 +0330

    Reduce spacing between chart and legend by 50% for Battery SOC vs revenue times chart

--- src/components/charts/SOCRevenueChart.jsx
+++ src/components/charts/SOCRevenueChart.jsx
@@ -87,7 +87,7 @@ function SOCRevenueChart({ data }) {
           <ResponsiveContainer width="100%" height={200}>
             <ComposedChart
               data={chartData}
-              margin={{ top: 20, right: 30, left: 20, bottom: 20 }}
+              margin={{ top: 20, right: 30, left: 20, bottom: 10 }}
             >
               <CartesianGrid 
                 strokeDasharray="0" 
